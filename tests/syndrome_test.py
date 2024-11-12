f28ca688aac8c440350f7e3e78ce17017d0e6322
Benjamin DalFavero <bdalfavero@gmail.com>
2024-11-12 16:38:36 -0500

    Added a test case.

--- tests/syndrome_test.py
+++ tests/syndrome_test.py
@@ -29,6 +29,29 @@ class TestErrorToSyndrome(unittest.TestCase):
         z_syndrome_target = [False] * 3 + [True] * 3
         x_syndrome_target = [False] * 6
         self.assertTrue(z_syndrome == z_syndrome_target and x_syndrome == x_syndrome_target)
+    
+    def test_y_middle(self):
+        q = cirq.GridQubit(2, 2)
+        err = cirq.PauliString({q: cirq.Y})
+        z_syndrome, x_syndrome = error_to_syndrome(3, err)
+        z_syndrome_target = [False, True, False, False, True, False]
+        x_syndrome_target = [False, False, True, True, False, False]
+        self.assertTrue(z_syndrome == z_syndrome_target and x_syndrome == x_syndrome_target)
+    
+    def test_neighboring_x_cancels(self):
+        # If there are X errors on two qubits for a stabilizer, then 
+        # that stabilizer should register nothing. However, the 
+        # next one will trigger.
+        # For example, consider the error XXI,II,III,II,III.
+        # The top-row x stabilizers should register [False, True].
+        q1 = cirq.GridQubit(0, 0)
+        q2 = cirq.GridQubit(0, 2)
+        err = cirq.PauliString({q1: cirq.X, q2: cirq.X})
+        z_syndrome, x_syndrome = error_to_syndrome(3, err)
+        print(z_syndrome, x_syndrome)
+        z_syndrome_target = [False] * 6
+        x_syndrome_target = [False, True] + [False] * 4
+        self.assertTrue(z_syndrome == z_syndrome_target and x_syndrome == x_syndrome_target)
 
 if __name__ == "__main__":
     unittest.main()
