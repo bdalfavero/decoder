eb0b76eca314fdfa47df4ebb29b345b98d236157
Benjamin DalFavero <bdalfavero@gmail.com>
2024-11-12 16:28:22 -0500

    For future use. Class for mapping surface code syndrome to their data qubits.

---
+++ decoder/surface_code.py
@@ -0,0 +1,11 @@
+import cirq
+
+class SurfaceCode:
+
+    def __init__(self, d) -> None:
+        """Initialize the SurfaceCode object.
+        
+        Arguments:
+        d - distance of the code."""
+
+        pass
