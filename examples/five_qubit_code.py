e7e5539c143a4535ce94cdada53d2d4d4dc7e597
Benjamin DalFavero <bdalfavero@gmail.com>
2025-03-19 10:16:29 -0400

    Compared pymatching and Chubb's deocder at low number of shots. Results are comparable in order of magnitude, but the detector picture deocder seems to perform worse than MWPM.

--- examples/five_qubit_code.py
+++ examples/five_qubit_code.py
@@ -1,6 +1,7 @@
 import numpy as np
 import stim
 import pymatching
+from decoder.dem_decoder import DEMDecoder
 
 def five_qubit_ckt(p: float) -> stim.Circuit:
     """Get memory experiment circuit for the five qubit code
@@ -79,11 +80,91 @@ def test_ideal_ckt():
         assert all(np.invert(syndromes))
         assert not bool(sum(logicals) % 2)
 
-if __name__ == "__main__":
-    ckt = five_qubit_ckt(0.1)
-    detector_sampler = ckt.compile_detector_sampler()
-    detector_flips, observable_flips = detector_sampler.sample(shots=10, separate_observables=True)
-    dem = ckt.detector_error_model()
+
+def pymatching_callback(dem, detector_events) -> np.ndarray:
+    """Use pymatching to decode detector events."""
+
     matcher = pymatching.Matching.from_detector_error_model(dem)
-    predictions = [bool(b) for b in matcher.decode_batch(detector_flips)]
-    print(predictions, observable_flips[:, 0])
+    predictions = [bool(b) for b in matcher.decode_batch(detector_events)]
+    return predictions
+
+
+def dem_decoder_callback(dem, syndrome_events, p) -> np.ndarray:
+    """Use Chubb's detector picture decoder on the syndromes."""
+
+    # Prepare 5 qubit code deocder.
+    stabilizers = []
+    stabilizers.append(stim.PauliString("+XZZXI"))
+    stabilizers.append(stim.PauliString("+IXZZX"))
+    stabilizers.append(stim.PauliString("+XIXZZ"))
+    stabilizers.append(stim.PauliString("+ZXIXZ"))
+
+    logicals = []
+    logicals.append(stim.PauliString("+XZIZI"))
+    logicals.append(stim.PauliString("+IZIXX"))
+
+    # Give each qubit depolarizing noise.
+    single_q_prob = np.array([1.0 - p, p / 3.0, p / 3.0, p / 3.0])
+    probabilities = np.zeros((5, 4), dtype=float)
+    for i in range(5):
+        probabilities[i, :] = single_q_prob
+
+    decoder = DEMDecoder(probabilities, stabilizers, logicals)
+    classes =  decoder.decode_batch(syndrome_events)
+
+    flips: List[bool] = []
+    for err_class in classes:
+        if err_class == 1 or err_class == 2:
+            flips.append(True)
+        else:
+            flips.append(False)
+    return np.array(flips)
+
+
+def count_errors(p: float, shots: int, decoder_callback, use_syndrome=False) -> int:
+    """Simulate shots of the memory experiment, and count how many times
+    pymatching's prediction of the observable flip does not match the observed
+    observable flip.
+    
+    Arguments:
+    p - depolarizing error probability.
+    shots - number of shots to simulate
+    decoder_callback - callback mapping DEM and either syndromes or detectors to predictions.
+    use_syndrome - If true, uses syndromes for decoding. Otherwise, uses detectors."""
+
+    ckt = five_qubit_ckt(p)
+    dem = ckt.detector_error_model()
+    if use_syndrome:
+        detector_sampler = ckt.compile_detector_sampler()
+        measurement_sampler = ckt.compile_sampler()
+        detector_flips, observable_flips = detector_sampler.sample(shots=shots, separate_observables=True)
+        all_samples = measurement_sampler.sample(shots=shots)
+        syndrome_samples = all_samples[:, :4] # Only the first four columns are the syndrome.
+        predictions = decoder_callback(dem, syndrome_samples)
+    else:
+        detector_sampler = ckt.compile_detector_sampler()
+        detector_flips, observable_flips = detector_sampler.sample(shots=shots, separate_observables=True)
+        predictions = decoder_callback(dem, detector_flips)
+
+    num_errors = 0
+    for s in range(shots):
+        if predictions[s] != observable_flips[s]:
+            num_errors += 1
+    return num_errors
+
+if __name__ == "__main__":
+    shots = 1000
+    ps = np.linspace(1e-3, 1e-2, num=10)
+    pymatching_lers = []
+    dem_lers = []
+    for p in ps:
+        pymatching_num_errors = count_errors(p, shots, pymatching_callback)
+        pymatching_lers.append(float(pymatching_num_errors) / float(shots))
+        dem_num_errors = count_errors(
+            p, shots,
+            lambda dem, syndrome: dem_decoder_callback(dem, syndrome, p),
+            use_syndrome=True
+        )
+        dem_lers.append(float(dem_num_errors) / float(shots))
+    print(pymatching_lers)
+    print(dem_lers)
